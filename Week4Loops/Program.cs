2b34193da6111dfe59631afe2e9892d2d115b605
caprxquemini <stefaniabarabula12@gmail.com>
2023-05-21 17:31:18 +0300

    Add project files.

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Random rnd = new Random();
+
+
+
+for (int i = 0; i < 3; i++)
+
+{
+    int myrandom = rnd.Next(1, 11);
+    Console.WriteLine(myrandom);
+}
+
+//for (int i = 0; i < 10; i++)
+//{
+//  Console.WriteLine($"{i+1}. I WILL NOT SKIP ZOOM CLASS.");
+//}
---
+++ Week4WhileLoop/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Random rnd = new Random();
+
+int cpuRandom = rnd.Next(1, 4);
+
+bool loopActive = true;
+
+int numberOfTries = 0;
+
+while (numberOfTries < 3)
+{
+    Console.WriteLine("Guess the CPU number:");
+
+    int userGuess = Convert.ToInt32(Console.ReadLine());
+
+    numberOfTries++;
+
+    if (userGuess == cpuRandom)
+
+        Console.WriteLine("Congratulations! You won!");
+        loopActive = false;
+
+}
+
+Console.WriteLine($"Number of tries: {numberOfTries}.")
